a561280c762c7d08427f793513be5b8c67f13000
olha-babik <132265386+olha-babik@users.noreply.github.com>
2023-06-08 20:51:47 +0300

    saved js

---
+++ index.js
@@ -0,0 +1,59 @@
+let now = new Date();
+let h3 = document.querySelector("h3");
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+h3.innerHTML = `${day}, ${month} ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "a0594785269187bedc0d136b83b28dc4";
+  let city = document.querySelector("#search-text-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", search);
